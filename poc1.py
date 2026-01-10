b051d1ee65b23c9a3f4d1e962f28744f691a3673
Youhei Sakurai <sakurai.youhei@gmail.com>
2026-01-10 11:58:14 +0900

    WIP

--- poc1.py
+++ poc1.py
@@ -102,6 +102,7 @@ async def record_audio(fname: str):
         while np.linalg.norm(np.array(motion.get_user_acceleration())) < 1:
             print("Recording... ", getsize(fname), "bytes")
             await asyncio.sleep(0.1)
+            print("Re-recording...")
             recorder.pause()
             recorder.record()
     finally:
